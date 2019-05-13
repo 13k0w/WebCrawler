bfc80d1644189ac7ce5758607ec90b27a633ce62
13k0w <you@example.com>
2019-05-13 20:14:35 +0200

    Third commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
 options = Options()
-# options.add_argument("--headless")
+options.add_argument("--headless")
 browser = webdriver.Firefox(firefox_options=options)
 
 browser.get("https://www.google.com")
