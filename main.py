e81fc641e911e5a61a391816df2e8adbd1b76f24
13k0w <you@example.com>
2019-05-15 15:53:59 +0200

    Stefan da uce

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-print("LEKOV KLOSARISTE")
+import urllib.request # https://pythonprogramming.net/urllib-tutorial-python-3/
+import bs4 as bs # https://www.pythonforbeginners.com/beautifulsoup/beautifulsoup-4-python
 
-print("LEKOV SO E OVAA RABOTA BE NE BIVA")
+URL = "http://quotes.toscrape.com/page/{}/" # zaso na kraj pomegu dvete / ima {} -> https://www.geeksforgeeks.org/python-format-function/
+                                            # posle dole koristemo format
+
+req = urllib.request.urlopen(URL.format(1)) # praves http request do stranata i ona ti vraka odgovor, kojsto se zacuvue u req
+soup = bs.BeautifulSoup(req, "lxml") # lxml (u slucaj da te interesira) -> https://lxml.de/
+                                     # bs e skraeno od BeautifulSoup i se koriste za da mozemo polsno da trazemo elementi na stranata
+
+if(req.code == 200): # https://www.restapitutorial.com/httpstatuscodes.html
+    print("Success")
+else:
+    print("Error")
